d7cc7ae35760a78fc0cec490b2460bc77067d829
InGee Kim <ingee.kim@sk.com>
2024-11-21 12:34:50 +0900

    Ch6/SecretMessage: Add MessageFragment (WIP)

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class WelcomeFragment : Fragment() {
+class MessageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return inflater.inflate(R.layout.fragment_message, container, false)
     }
 }
