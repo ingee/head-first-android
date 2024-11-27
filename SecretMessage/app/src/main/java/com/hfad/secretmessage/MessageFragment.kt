4fdb8560bbc652b70e6ce1c725607f9b06db1656
InGee Kim <ingee.kim@gmail.com>
2024-11-27 14:03:52 +0900

    Ch6/SecretMessage: Add EcryptFragment

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+        nextButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+        return view
     }
 }
