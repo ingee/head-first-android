44c6fb8287d207d67b11bceac073052b13a29686
InGee Kim <ingee.kim@sk.com>
2024-11-29 14:04:51 +0900

    Ch7/SecretMessage: Add SafeArgs plugin

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
@@ -15,9 +16,13 @@ class MessageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_message, container, false)
         val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
+
         nextButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_messageFragment_to_encryptFragment)
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections
+                .actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
         }
         return view
     }
