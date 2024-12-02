4076c7f333ab31bb2518d657ee4f28959aa6caf3
InGee Kim <ingee.kim@sk.com>
2024-12-02 09:29:07 +0900

    Ch7/SecretMessage: Navigate to EncryptFragment

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +17,10 @@ class EncryptFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+        encryptedView.text = message.reversed()
+        return view
     }
 }
