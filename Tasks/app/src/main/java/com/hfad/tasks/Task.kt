878e85e8f24b1b3fd0edd9ba3da36208e991640a
InGee Kim <ingee.kim@gmail.com>
2025-01-23 07:43:32 +0900

    Ch14/Tasks: Make database

--- Tasks/app/src/main/java/com/hfad/tasks/Task.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/Task.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "task_talbe")
+@Entity(tableName = "task_table")
 data class Task (
     @PrimaryKey(autoGenerate = true)
     var taskId: Long = 0L,
