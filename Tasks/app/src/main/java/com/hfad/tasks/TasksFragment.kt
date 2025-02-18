3a7456f1fd2f2195f6989b2bcc5f7227311a98cf
InGee Kim <ingee.kim@sk.com>
2025-02-18 16:39:40 +0900

    Ch17/Tasks: Navigate from TasksFragment to EidtTaskFragment

--- Tasks/app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -1,13 +1,13 @@
 package com.hfad.tasks
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.hfad.tasks.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
@@ -30,12 +30,22 @@ class TasksFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = TaskItemAdapter { taskId ->
-            Toast.makeText(context, "Clicked task $taskId", Toast.LENGTH_SHORT).show()
+            viewModel.onTaskClicked(taskId)
         }
         binding.tasksList.adapter = adapter
+
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner, Observer { taskId ->
+            taskId?.let {
+                val action = TasksFragmentDirections
+                    .actionTasksFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
             }
         })
         return view
