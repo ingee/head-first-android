08320684173d8bb4f49c835992b40da4d739ddea
InGee Kim <ingee.kim@sk.com>
2025-02-21 14:20:36 +0900

    Ch18/TemperatureConverter: Show image and text

--- TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -3,30 +3,66 @@ package com.hfad.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            PreviewMainActivity()
+            MainActivityContent()
         }
     }
 }
 
 @Composable
-fun Hello(name: String) {
-    Text("Hello $name! Welcome to Jetpack Compose.")
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = description,
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TemperateText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble()*9/5)+32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun MainActivityContent() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Header(R.drawable.sunrise, "sunise image")
+            TemperateText(0)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
-    Column {
-        Hello("friend")
-        Hello("everyone")
-    }
+    MainActivityContent()
 }
