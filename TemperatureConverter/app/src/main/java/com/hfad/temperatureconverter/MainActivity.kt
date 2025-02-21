9b0c3d3b87f2f5e429e578a8a4221658443f57cc
InGee Kim <ingee.kim@sk.com>
2025-02-21 20:24:08 +0900

    Ch18/TemperatureConverter: Update composable text on button click

--- TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -5,16 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -32,6 +33,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TemperateText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble()*9/5)+32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+    }
+}
+
+@Composable
 fun Header(image: Int, description: String) {
     Image(
         painter = painterResource(image),
@@ -44,19 +58,16 @@ fun Header(image: Int, description: String) {
 }
 
 @Composable
-fun TemperateText(celsius: Int) {
-    val fahrenheit = (celsius.toDouble()*9/5)+32
-    Text("$celsius Celsius is $fahrenheit Fahrenheit")
-}
-
-@Composable
 fun MainActivityContent() {
+    val celsius = remember { mutableStateOf(0) }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
             Header(R.drawable.sunrise, "sunise image")
-            TemperateText(0)
+            ConvertButton { celsius.value = 20 }
+            TemperateText(celsius.value)
         }
     }
 }
